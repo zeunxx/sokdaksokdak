dd5227a4045da6a1a16021d0a762c6da1841e32a
ro-el-c <chung3286@naver.com>
2022-11-22 17:11:00 +0900

    ro-el/ Docs: modify TODO

--- app/src/main/java/com/example/sokdaksokdak/writeDiary/DiaryFragment.kt
+++ app/src/main/java/com/example/sokdaksokdak/writeDiary/DiaryFragment.kt
@@ -30,13 +30,15 @@ class DiaryFragment : Fragment() {
         // writeDiaryViewModel.deleteData()
 
         // TODO: 사용자의 keyword 추천 여부 반영 - 예외 처리
-        // TODO: 앱 실행 시, 현재 날짜에 대한 DB Data 존재 및 작성 완료 상태 확인
-        //       -> 존재하지 않을 때
-        //          -> insertData
-        //          -> 존재할 때의 순서 시행
-        //       -> 존재
-        //          -> keyword & 내용 모두 작성 완료된 상태인지 확인
-        //          -> keyword 만 갱신 된 상태일 때
+
+        // 앱 실행 시, 현재 날짜에 대한 DB Data 존재 및 작성 완료 상태 확인
+        // -> 존재하지 않을 때
+        //    -> insertData
+        //    => 존재할 때의 순서 시행
+        // -> 존재
+        //    -> keyword & 내용 모두 작성 완료된 상태인지 확인
+        //    -> keyword 만 갱신 된 상태일 때
+        // TODO: 사용자가 keyword 를 수정했을 때
 
 
         // checkDataExists
@@ -50,7 +52,10 @@ class DiaryFragment : Fragment() {
 
 
         // showKeyword
-        // TODO: keyword & 내용 모두 작성 완료된 상태인지 확인
+        /**
+         * keyword & 내용 모두 작성 완료된 상태라면
+         * EditText 가 아닌 TextView 로 화면에 표시
+         * */
         if (writeDiaryViewModel.checkDiaryCompleted()){
             binding.diaryDoneBtn.visibility = View.GONE
             binding.diaryTextView.visibility = View.VISIBLE
@@ -58,14 +63,18 @@ class DiaryFragment : Fragment() {
             binding.diaryTextView.setText(writeDiaryViewModel.showContent())
         }
 
-        // TODO: keyword 만 갱신 된 상태일 때
+        // showKeyword
+        // TODO: 사용자가 keyword 수정/직접 입력했을 때
         /**
+         * keyword 만 갱신 된 상태일 때
+         *
          * 1. 오늘 날짜에 해당하는 keyword 가져오기
          * 2. 가져온 keyword 가
          *    2.1. 초기 값일 때 - random 으로 키워드 가져오기
          *         2.1.1. 가져온 keyword DB 에 update
          *    2.2. 이미 갱신된 값일 때 - 바로 반환
          * 3. 화면에 keyword 표시
+         *
          * */
 
         binding.keywordTextView.text = writeDiaryViewModel.showKeyword()
@@ -90,8 +99,6 @@ class DiaryFragment : Fragment() {
 
         }
 
-
-        // Inflate the layout for this fragment
         return binding.root
     }
 
